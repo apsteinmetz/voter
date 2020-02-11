f6c5ee3295bd31440536484521520ff86987efba
apsteinmetz <apsteinmetz@yahoo.com>
2020-02-11 10:52:36 -0500

    zipped up files. divided by party. done.

--- make_3d_bars.R
+++ make_3d_bars.R
@@ -7,7 +7,7 @@ library(rayshader)
 #install.packages("rgl", repos = "http://R-Forge.R-project.org", type = "source")
 
 plot_ggg <- function(save_height_matrix = FALSE){
-  ret_val <- plot_gg(p, width = 4, height = 3, scale = 150, 
+  ret_val <- plot_gg(p, width = 4, height = 3, scale = 50, 
                      multicore = TRUE, 
                      raytrace = FALSE,
                      reduce_size = 0.5,
@@ -31,18 +31,19 @@ load("data/voter_18_scaled.rdata")
 view_grid <- voter_18_scaled %>% 
   group_by(caseid,party_2018) %>% 
   summarise(social = mean(social,na.rm = TRUE),fiscal = mean(fiscal,na.rm = TRUE)) %>%
-  na.omit() %>% 
-# p <- view_grid %>%  
-#   round(digits=1) %>% 
-# #  ggplot(aes(social,fiscal,fill=n)) + geom_tile() + 
-#   ggplot(aes(social,fiscal)) + 
-#   geom_hex(bins = 8,color="black") +   
-#   scale_fill_viridis_c(option = "C") +
-#   labs(title = "The Nation Is Not Polarized",
-#        subtitle = "YouGov 2018 Voter Survey",
-#        y = "More Fiscally Conservative --->",
-#        x = "More Socially Conservative --->") + 
-#   NULL
+  na.omit() 
+
+  # p <- view_grid %>%  
+  #   round(digits=1) %>% 
+  # #  ggplot(aes(social,fiscal,fill=n)) + geom_tile() + 
+  #   ggplot(aes(social,fiscal)) + 
+  #   geom_hex(bins = 8,color="black") +   
+  #   scale_fill_viridis_c(option = "C") +
+  #   labs(title = "The Nation Is Polarized",
+  #        subtitle = "YouGov 2018 Voter Survey",
+  #        y = "More Fiscally Conservative --->",
+  #        x = "More Socially Conservative --->") + 
+  #   NULL
 # 
 # p
 
@@ -51,14 +52,14 @@ col_scheme = "D"
 p <- view_grid %>%   
   ggplot() +
   stat_density_2d(aes(x = social, y = fiscal, fill = stat(nlevel)), 
-                  geom = "polygon", bins = 10, contour = TRUE) +
-  #  facet_wrap(clarity~.) +
+                  geom = "polygon", bins = 10, contour = TRUE)+
+  facet_wrap(party_2018~.) +
   scale_fill_viridis_c(option = col_scheme, name = "Relative\nNumber\nof Voters") +
   labs(title = "The Nation is Polarized",
-       subtitle = "YouGov 2018 Voter Attitude Survey",
+       subtitle = "YouGov.com 2018 Voter Attitude Survey by Party Affiliation",
        y = "More Fiscally Conservative --->",
        x = "More Socially Conservative --->") + 
-#  facet_wrap(~party_2018) + 
+  #  facet_wrap(~party_2018) + 
   NULL
 p
 
@@ -78,7 +79,45 @@ render_camera(
   theta = 0, 
   phi = 45)
 render_snapshot()
+# ------------------------------------------------
+total_frames = 5 + 45 +  360  + 45 # tilt then rotate then untilt
+for (n in 411:total_frames) {
+  if (n < 5) {
+    phi <- 90
+    theta = 0
+  }
+  if (n >4  &  n < 45) {
+    phi <- 90 - n
+    theta = 0
+  }
+  if ( n > 44){
+    phi = 45
+    theta = n - 45
+  }
+  if ( n > 410){
+    phi = n - 365
+    theta = 0
+  }
+  print(paste("frame",n))  
+  render_camera(
+    fov = 70, 
+    zoom = 0.6, 
+    theta = theta, 
+    phi = phi)
+  render_snapshot(filename = paste0("img/voter_view_",
+                                    str_replace_all(format(n,width = nchar(total_frames))," ","0"),
+                                    ".png"))
+  
+}
 
 
+# ------------------------------------------------
+library(magick)
+capturas <- paste0("img/",list.files("img/", pattern = "\\.png$"))
 
+# get all images in a list
+images <- map(capturas, image_read)
+images <- image_join(images)
+
+voter_anim <- image_animate(images, fps = 2, dispose = "previous")
 
--- scratchpad.R
+++ scratchpad.R
@@ -1,10 +1,15 @@
 library(ggplot2)
 library(rayshader)
-ggdiamonds = ggplot(diamonds) +
-  stat_density_2d(aes(x = x, y = depth, fill = stat(nlevel)), 
+
+voter_18_scaled %>% na.omit()
+
+ggdiamonds = ggplot(view_grid) +
+  stat_density_2d(aes(x = social, y = fiscal, fill = stat(nlevel)), 
                   geom = "polygon", n = 100, bins = 10, contour = TRUE) +
-  facet_wrap(clarity~.) +
-  scale_fill_viridis_c(option = "A")
+#  facet_wrap(clarity~.) +
+  scale_fill_viridis_c(option = "D")
+
+ggdiamonds
 
 par(mfrow = c(1, 2))
 
@@ -13,3 +18,5 @@ plot_gg(ggdiamonds, width = 5, height = 5, multicore = TRUE, scale = 250,
         zoom = 0.7, theta = 10, phi = 30, windowsize = c(800, 800))
 Sys.sleep(0.2)
 render_snapshot(clear = TRUE)
+
+             
